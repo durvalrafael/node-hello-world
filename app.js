63d70090bf0dafcb88880e1e362468915e7511c1
Durval Moreira <durvalmoreira@durvalmacbook.local>
2018-03-16 23:24:00 -0300

    #

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
+app.get('/hello', function (req, res) {
+	res.send({
+		message: 'Hello World!'
+	});
 });
 
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
 });
+
+module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,31 @@
+process.env.NODE_ENV = 'test';
+
 const assert = require('assert');
-describe('Array', function () {
-	describe('#indexOf()', function () {
-		it('should return -1 when the value is not present', function () {
-			assert.equal([1, 2, 3].indexOf(4), -1);
-		});
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+const should = chai.should();
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
+describe('/GET hello', () => {
+	it('it should GET hello world string', (done) => {
+		chai.request(app)
+			.get('/hello')
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				res.body.should.should.be.a('object');
+				res.body.should.have.property('message');
+				res.body.should.have.property('message').equal('Hello World!');
+				done();
+			});
 	});
 });
